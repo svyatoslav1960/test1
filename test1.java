2df5869acd1d8a24153fc288dbaf84a027260ad6
svyatoslav1960 <slava.makagonov@gmail.com>
2021-11-01 12:14:14 +0100

    renamed fields & accessors

--- test1.java
+++ test1.java
@@ -1,12 +1,12 @@
 class A {
-  private int x;
-  private int y;
+  private int x1;
+  private int y1;
 
-  public void setX(int val) {
-    this.y = val;
+  public void setX1(int val) {
+    this.y1 = val;
   }
 // just a comment for testing in temp1
-  public int getY() {
-    return this.x;
+  public int getY1() {
+    return this.x1;
   }
 }
